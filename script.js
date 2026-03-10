344e3211635d29644345dd776989b9d98bf33a73
Tony Nguyen <tonynguyen0326@gmail.com>
2026-03-10 01:30:21 -0600

    added ability to publish messages to topics

--- script.js
+++ script.js
@@ -17,8 +17,9 @@ function startConnect() {
     // give client random numbers for ID
     client = new Paho.MQTT.Client(host, port, "client_" + Math.random());
 
-    // set callback handler for lost connection
+    // set callback handlers
     client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
     
     // connect client
     client.connect({onSuccess: onConnect});
@@ -32,11 +33,18 @@ function startConnect() {
 function onConnect() {
     // send message
     console.log("Connected to MQTT message broker!");
+
+    // enable ability to choose topic and send message
+
+    document.getElementById("topic").disabled = false;
+    document.getElementById("message").disabled = false;
+    document.getElementById("PublishButton").disabled = false;
 }
 
 // function to end connection
 function endConnect() {
 
+    // disconnect client
     if (client && client.isConnected()) {
         client.disconnect();
         console.log("Disconnected from MQTT message broker.");
@@ -44,16 +52,20 @@ function endConnect() {
 
     // enable changing inputs
     document.getElementById("host").disabled = false;
-    document.getElementById("port").disabled = false;    
+    document.getElementById("port").disabled = false; 
+    
+    // disable ability to choose topic and send message
+    document.getElementById("topic").disabled = true;
+    document.getElementById("message").disabled = true;
+    document.getElementById("PublishButton").disabled = true;
 }
 
 // function when client loses connection
 function onConnectionLost(responseObject) {
     // send message
     if (responseObject.errorCode !== 0) {
-        console.log("Connection Lost: " + responseObject.errorMessage);
+        console.log("onConnectionLost: " + responseObject.errorMessage);
     
-
     // send message about attempted reconnection
     alert("Attempting to reconnect!");
     
@@ -61,4 +73,34 @@ function onConnectionLost(responseObject) {
     setTimeout(startConnect, 3000);
 
     }
+}
+
+// function to publish message
+function publishMessage() {
+
+    // get topic and message from user
+    var topic = document.getElementById("topic").value;
+    var message = document.getElementById("message").value;
+
+    // make sure user is connected to broker
+    if (!client || !client.isConnected()) {
+        alert("A host broker and a port number need to be entered to send a message.");
+        return
+    }
+
+    // make sure both topic and message entered
+    if (!topic || !message) {
+        alert("Please enter both a topic and a message.");
+        return
+    }
+
+    // send message
+    var mqtt = new Paho.MQTT.Message(message);
+    mqtt.destinationName = topic;
+    client.send(mqtt);
+}
+
+// function for when message arrived
+function onMessageArrived(mqtt) {
+    console.log("onMessageArrived: " + mqtt.payloadString);
 }
